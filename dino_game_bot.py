2d01f31c63b6f28b89de0603915258f24db3bd9f
Ameer Mushani <ameermushani@gmail.com>
2018-02-12 23:14:03 -0500

    Version 2

--- dino_game_bot.py
+++ dino_game_bot.py
@@ -1,30 +1,44 @@
-import pyautogui , webbrowser, selenium
+import pyautogui, selenium
 from splinter import Browser
 def jump():
     pyautogui.typewrite(" ")
-    return
 def isObstacleClose():
     xPos = browser.evaluate_script('Runner.instance_.horizon.obstacles[0].xPos')
-    if(xPos < 105):
+    if(xPos < 120):
         return True
     else:
         return False
-
+undefinedX = True
+groundY = 23
+trexX = 93
+trexWidth = 44
 browser = Browser('chrome')
 browser.visit('chrome://dino')
 jump()
-foundX = False
-while foundX != True:
-    try:
-        xpos = browser.evaluate_script('Runner.instance_.horizon.obstacles[0].xPos')
-        foundX = True;
-    except selenium.common.exceptions.WebDriverException:
-        pass
-print(xpos)
 while True:
-    if (isObstacleClose()):
-        if(browser.evaluate_script('Runner.instance_.horizon.obstacles[0].yPos') != 50):
+    if browser.evaluate_script('Runner.instance_.crashed'):
+        while True:
+            print ("dead")
+    if undefinedX:
+        try:
+            xpos = browser.evaluate_script('Runner.instance_.horizon.obstacles[0].xPos')
+            undefinedX = False;
+        except selenium.common.exceptions.WebDriverException:
+            pass
+    else:
+        if browser.evaluate_script('Runner.instance_.crashed'):
+            undefinedX = True
             jump()
-    
-
-
+        else:
+            if (isObstacleClose()):
+                if(browser.evaluate_script('Runner.instance_.horizon.obstacles[0].yPos') > 50):
+                    jump()
+                    while browser.evaluate_script('Runner.instance_.tRex.jumping'):
+                        print(browser.evaluate_script('Runner.instance_.horizon.obstacles[0].xPos') + browser.evaluate_script('Runner.instance_.horizon.obstacles[0].width'))
+                        print(browser.evaluate_script('Runner.instance_.horizon.obstacles[0].xPos'))
+                        
+                        if(browser.evaluate_script('Runner.instance_.horizon.obstacles[0].xPos') + browser.evaluate_script('Runner.instance_.horizon.obstacles[0].width') < trexX - trexWidth):
+                            pyautogui.keyDown('down')
+                    pyautogui.keyUp('down')
+##                    if(browser.evaluate_script('Runner.instance_.horizon.obstacles[0]
+##                    print(browser.evaluate_script('Runner.instance_.horizon.obstacles[0].yPos'))
